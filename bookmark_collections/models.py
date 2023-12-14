7720087bc871cb8437a1181860bcbf02be84688e
DorLex <dorozhinskiysanya@gmail.com>
2023-12-14 20:17:30 +0300

    add apps

---
+++ bookmark_collections/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Collections(models.Model):
+
+    def __str__(self):
+        return None
---
+++ bookmarks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Bookmark(models.Model):
+
+    def __str__(self):
+        return None
