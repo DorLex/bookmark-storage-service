d047bff1b26cb4e5c394b7113ab61a400dee06f1
DorLex <dorozhinskiysanya@gmail.com>
2023-12-18 19:28:32 +0300

    add bookmark to collection

--- bookmarks/views/bookmark.py
+++ bookmarks/views/bookmark.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from bookmark_collections.models import Collections
 from bookmarks.models import Bookmark
 from bookmarks.serializers import BookmarkSerializer
 
@@ -15,6 +16,20 @@ class BookmarkAPIView(APIView):
         """Получить закладку"""
 
         bookmark = get_object_or_404(Bookmark, user=request.user, pk=bookmark_id)
+        serializer = BookmarkSerializer(bookmark)
+
+        return Response(serializer.data)
+
+    def post(self, request, bookmark_id):
+        """Добавить закладку в коллекцию"""
+
+        collection_id = request.data.get('collection_id')
+
+        bookmark = get_object_or_404(Bookmark, user=request.user, pk=bookmark_id)
+        collection = get_object_or_404(Collections, user=request.user, pk=collection_id)
+
+        bookmark.collections.add(collection)
+
         serializer = BookmarkSerializer(bookmark)
 
         return Response(serializer.data)
