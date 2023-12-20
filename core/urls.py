6e6c8782bcebab4bda24d94a22bd6893d9c5fc93
DorLex <dorozhinskiysanya@gmail.com>
2023-12-21 00:26:26 +0300

    refact

--- core/urls.py
+++ core/urls.py
@@ -19,8 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
